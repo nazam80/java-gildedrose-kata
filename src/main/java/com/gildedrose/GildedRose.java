111949ced0755898cdaa03259f52696851293178
Antonio Martínez López <nazam80@gmail.com>
2019-12-09 14:55:22 +0100

    Reorder if structure to get similar code together

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -37,21 +37,19 @@ public class GildedRose {
             }else if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert") ) {
     			if (item.getQuality() < 50) {
                     item.setQuality(item.getQuality() + 1);
-                    if (item.getSellIn() < 11) {     
-                    	if (item.getQuality() < 50) {
-                            item.setQuality(item.getQuality() + 1);
-                    	}
-                        
-                    }
-
-                    if (item.getSellIn() < 6) {      
-                    	if (item.getQuality() < 50) {
-                            item.setQuality(item.getQuality() + 1);
-                    	}
-                    	item.setSellIn(item.getSellIn() - 1);
-                    }
-                    
     			}
+    			if (item.getSellIn() < 11) {     
+                	if (item.getQuality() < 50) {
+                        item.setQuality(item.getQuality() + 1);
+                	}
+                    
+                }
+    			if (item.getSellIn() < 6) {      
+                	if (item.getQuality() < 50) {
+                        item.setQuality(item.getQuality() + 1);
+                	}                	
+                }
+    			
     			item.setSellIn(item.getSellIn() - 1);
     			if (item.getSellIn() < 0) {
                 	item.setQuality(item.getQuality()
