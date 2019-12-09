009382f48a2f066578308732bd395a03ccaf58f2
Antonio Martínez López <nazam80@gmail.com>
2019-12-09 14:53:54 +0100

    Reorder if with first if comparing by name

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -21,56 +21,54 @@ public class GildedRose {
     }
 
     public void updateQuality() {
-    	for (Item item : items) {
-            if (!item.getName().equals("Aged Brie")
-                    && !item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.getQuality() > 0) {
-                    if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                        item.setQuality(item.getQuality() - 1);
-                    }
-                }
-            } else {
-                if (item.getQuality() < 50) {
+    	for (Item item : items) {    		
+            if (item.getName().equals("Aged Brie") ) {
+            	if (item.getQuality() < 50) {
                     item.setQuality(item.getQuality() + 1);
-
-                    if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert") ){
-                        if (item.getSellIn() < 11) {
-                            if (item.getQuality() < 50) {
-                                item.setQuality(item.getQuality() + 1);
-                            }
-                        }
-
-                        if (item.getSellIn() < 6) {
-                            if (item.getQuality() < 50) {
-                                item.setQuality(item.getQuality() + 1);
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                item.setSellIn(item.getSellIn() - 1);
-            }
-
-            if (item.getSellIn() < 0) {
-                if (!item.getName().equals("Aged Brie")) {
-                    if (!item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.getQuality() > 0) {
-                            if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                                item.setQuality(item.getQuality() - 1);
-                            }
-                        }
-                    } else {
-                        item.setQuality(item.getQuality()
-                                - item.getQuality());
-                    }
-                } else {
-                    if (item.getQuality() < 50) {
+    			}
+            	item.setSellIn(item.getSellIn() - 1);
+    			if (item.getSellIn() < 0) {
+    				if (item.getQuality() < 50) {
                         item.setQuality(item.getQuality() + 1);
                     }
+    				
+    			}
+    			
+            }else if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert") ) {
+    			if (item.getQuality() < 50) {
+                    item.setQuality(item.getQuality() + 1);
+                    if (item.getSellIn() < 11) {     
+                    	if (item.getQuality() < 50) {
+                            item.setQuality(item.getQuality() + 1);
+                    	}
+                        
+                    }
+
+                    if (item.getSellIn() < 6) {      
+                    	if (item.getQuality() < 50) {
+                            item.setQuality(item.getQuality() + 1);
+                    	}
+                    	item.setSellIn(item.getSellIn() - 1);
+                    }
+                    
+    			}
+    			item.setSellIn(item.getSellIn() - 1);
+    			if (item.getSellIn() < 0) {
+                	item.setQuality(item.getQuality()
+                            - item.getQuality());
                 }
-            }
+    		}else if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.getQuality() > 0) {
+                	 item.setQuality(item.getQuality() - 1);                
+                }
+                item.setSellIn(item.getSellIn() - 1);
+                if (item.getSellIn() < 0) {
+                	if (item.getQuality() > 0) {
+                		item.setQuality(item.getQuality() - 1);
+                	}
+                }
+    		}
+            
         }
     }
 }
